6ad01c974c72c5d4aa7c3b2049f51e8132d9bea6
Akexorcist <akexorcist@gmail.com>
2015-11-30 10:47:46 +0700

    Remove logging

--- googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/network/DirectionAndPlaceConnection.java
+++ googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/network/DirectionAndPlaceConnection.java
@@ -19,8 +19,6 @@ limitations under the License.
 package com.akexorcist.googledirection.network;
 
 import com.akexorcist.googledirection.constant.DirectionUrl;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
@@ -32,14 +30,8 @@ public class DirectionAndPlaceConnection {
     private static DirectionAndPlaceService service;
     public static DirectionAndPlaceService createService() {
         if(service == null) {
-            OkHttpClient client = new OkHttpClient();
-            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            client.interceptors().add(interceptor);
-
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(DirectionUrl.MAPS_API_URL)
-                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
             service = retrofit.create(DirectionAndPlaceService.class);
