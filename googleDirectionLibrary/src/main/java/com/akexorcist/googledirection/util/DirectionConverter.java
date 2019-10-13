2bc3cac6e1ae2d478d79c8794e4b9fedb98a842c
Akexorcist <akexorcist@gmail.com>
2019-10-13 23:31:17 +0700

    Provide more polyline customizable close #60

--- googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/util/DirectionConverter.java
+++ googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/util/DirectionConverter.java
@@ -21,12 +21,23 @@ package com.akexorcist.googledirection.util;
 import android.content.Context;
 import android.util.DisplayMetrics;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.Dimension;
+import androidx.annotation.IntRange;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.akexorcist.googledirection.model.Step;
+import com.google.android.gms.maps.model.Cap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PatternItem;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static androidx.annotation.Dimension.DP;
 
 /**
  * Created by Akexorcist on 11/29/15 AD.
@@ -50,9 +61,7 @@ public class DirectionConverter {
         if (step.getPolyline() != null) {
             List<LatLng> decodedPointList = step.getPolyline().getPointList();
             if (decodedPointList != null && decodedPointList.size() > 0) {
-                for (LatLng position : decodedPointList) {
-                    directionPointList.add(position);
-                }
+                directionPointList.addAll(decodedPointList);
             }
         }
 
@@ -73,24 +82,341 @@ public class DirectionConverter {
         return directionPointList;
     }
 
-    public static PolylineOptions createPolyline(Context context, ArrayList<LatLng> locationList, int width, int color) {
+    public static PolylineOptions createPolyline(
+            @NonNull Context context,
+            @NonNull PathOption pathOption
+    ) {
+        return createPolyline(
+                context,
+                pathOption.locationList,
+                pathOption.width,
+                pathOption.color,
+                pathOption.clickable,
+                pathOption.jointType,
+                pathOption.startCap,
+                pathOption.endCap,
+                pathOption.patternItemList
+        );
+    }
+
+    public static PolylineOptions createPolyline(
+            @NonNull Context context,
+            @Nullable ArrayList<LatLng> locationList,
+            @Dimension(unit = DP) int width,
+            @ColorInt int color
+    ) {
+        return createPolyline(
+                context,
+                locationList,
+                width,
+                color,
+                true,
+                JointType.DEFAULT,
+                null,
+                null,
+                null
+        );
+    }
+
+    public static PolylineOptions createPolyline(
+            @NonNull Context context,
+            @Nullable ArrayList<LatLng> locationList,
+            @Dimension(unit = DP) int width,
+            @ColorInt int color,
+            boolean clickable
+    ) {
+        return createPolyline(
+                context,
+                locationList,
+                width,
+                color,
+                clickable,
+                JointType.DEFAULT,
+                null,
+                null,
+                null
+        );
+    }
+
+    public static PolylineOptions createPolyline(
+            @NonNull Context context,
+            @Nullable ArrayList<LatLng> locationList,
+            @Dimension(unit = DP) int width,
+            @ColorInt int color,
+            boolean clickable,
+            @IntRange(from = JointType.DEFAULT, to = JointType.ROUND) int jointType
+    ) {
+        return createPolyline(
+                context,
+                locationList,
+                width,
+                color,
+                clickable,
+                jointType,
+                null,
+                null,
+                null
+        );
+    }
+
+    public static PolylineOptions createPolyline(
+            @NonNull Context context,
+            @Nullable ArrayList<LatLng> locationList,
+            @Dimension(unit = DP) int width,
+            @ColorInt int color,
+            boolean clickable,
+            @Nullable Cap startCap,
+            @Nullable Cap endCap
+    ) {
+        return createPolyline(
+                context,
+                locationList,
+                width,
+                color,
+                clickable,
+                JointType.DEFAULT,
+                startCap,
+                endCap,
+                null
+        );
+    }
+
+    public static PolylineOptions createPolyline(
+            @NonNull Context context,
+            @Nullable ArrayList<LatLng> locationList,
+            @Dimension(unit = DP) int width,
+            @ColorInt int color,
+            boolean clickable,
+            @Nullable List<PatternItem> patternItemList
+    ) {
+        return createPolyline(
+                context,
+                locationList,
+                width,
+                color,
+                clickable,
+                JointType.DEFAULT,
+                null,
+                null,
+                patternItemList
+        );
+    }
+
+    public static PolylineOptions createPolyline(
+            @NonNull Context context,
+            @Nullable ArrayList<LatLng> locationList,
+            @Dimension(unit = DP) int width,
+            @ColorInt int color,
+            boolean clickable,
+            @IntRange(from = JointType.DEFAULT, to = JointType.ROUND) int jointType,
+            @Nullable Cap startCap,
+            @Nullable Cap endCap,
+            @Nullable List<PatternItem> patternItemList) {
         PolylineOptions rectLine = new PolylineOptions().width(dpToPx(context, width)).color(color).geodesic(true);
-        for (LatLng location : locationList) {
-            rectLine.add(location);
+        rectLine.clickable(clickable);
+        rectLine.jointType(jointType);
+        if (patternItemList != null) {
+            rectLine.pattern(patternItemList);
+        }
+        if (startCap != null) {
+            rectLine.startCap(startCap);
+        }
+        if (endCap != null) {
+            rectLine.endCap(endCap);
+        }
+        if (locationList != null && locationList.size() > 0) {
+            for (LatLng location : locationList) {
+                rectLine.add(location);
+            }
         }
         return rectLine;
     }
 
-    public static ArrayList<PolylineOptions> createTransitPolyline(Context context, List<Step> stepList, int transitWidth, int transitColor, int walkingWidth, int walkingColor) {
+    public static ArrayList<PolylineOptions> createTransitPolyline(
+            @NonNull Context context,
+            @NonNull TransitPathOption transitPathOption) {
+        return createTransitPolyline(
+                context,
+                transitPathOption.stepList,
+                transitPathOption.transitWidth,
+                transitPathOption.transitColor,
+                transitPathOption.transitPatternItemList,
+                transitPathOption.walkingWidth,
+                transitPathOption.walkingColor,
+                transitPathOption.walkingPatternItemList,
+                transitPathOption.clickable,
+                transitPathOption.jointType,
+                transitPathOption.startCap,
+                transitPathOption.endCap
+        );
+    }
+
+    public static ArrayList<PolylineOptions> createTransitPolyline(
+            @NonNull Context context,
+            @Nullable List<Step> stepList,
+            @Dimension(unit = DP) int transitWidth,
+            @ColorInt int transitColor,
+            @Dimension(unit = DP) int walkingWidth,
+            @ColorInt int walkingColor) {
+        return createTransitPolyline(
+                context,
+                stepList,
+                transitWidth,
+                transitColor,
+                null,
+                walkingWidth,
+                walkingColor,
+                null,
+                true,
+                JointType.DEFAULT,
+                null,
+                null
+        );
+    }
+
+    public static ArrayList<PolylineOptions> createTransitPolyline(
+            @NonNull Context context,
+            @Nullable List<Step> stepList,
+            @Dimension(unit = DP) int transitWidth,
+            @Nullable List<PatternItem> transitPatternItemList,
+            @ColorInt int transitColor,
+            @Dimension(unit = DP) int walkingWidth,
+            @ColorInt int walkingColor,
+            @Nullable List<PatternItem> walkingPatternItemList) {
+        return createTransitPolyline(
+                context,
+                stepList,
+                transitWidth,
+                transitColor,
+                transitPatternItemList,
+                walkingWidth,
+                walkingColor,
+                walkingPatternItemList,
+                true,
+                JointType.DEFAULT,
+                null,
+                null
+        );
+    }
+
+    public static ArrayList<PolylineOptions> createTransitPolyline(
+            @NonNull Context context,
+            @Nullable List<Step> stepList,
+            @Dimension(unit = DP) int transitWidth,
+            @ColorInt int transitColor,
+            @Dimension(unit = DP) int walkingWidth,
+            @ColorInt int walkingColor,
+            @Nullable boolean clickable) {
+        return createTransitPolyline(
+                context,
+                stepList,
+                transitWidth,
+                transitColor,
+                null,
+                walkingWidth,
+                walkingColor,
+                null,
+                clickable,
+                JointType.DEFAULT,
+                null,
+                null
+        );
+    }
+
+    public static ArrayList<PolylineOptions> createTransitPolyline(
+            @NonNull Context context,
+            @Nullable List<Step> stepList,
+            @Dimension(unit = DP) int transitWidth,
+            @ColorInt int transitColor,
+            @Dimension(unit = DP) int walkingWidth,
+            @ColorInt int walkingColor,
+            boolean clickable,
+            @IntRange(from = JointType.DEFAULT, to = JointType.ROUND) int jointType) {
+        return createTransitPolyline(
+                context,
+                stepList,
+                transitWidth,
+                transitColor,
+                null,
+                walkingWidth,
+                walkingColor,
+                null,
+                clickable,
+                jointType,
+                null,
+                null
+        );
+    }
+
+    public static ArrayList<PolylineOptions> createTransitPolyline(
+            @NonNull Context context,
+            @Nullable List<Step> stepList,
+            @Dimension(unit = DP) int transitWidth,
+            @ColorInt int transitColor,
+            @Dimension(unit = DP) int walkingWidth,
+            @ColorInt int walkingColor,
+            boolean clickable,
+            @Nullable Cap startCap,
+            @Nullable Cap endCap) {
+        return createTransitPolyline(
+                context,
+                stepList,
+                transitWidth,
+                transitColor,
+                null,
+                walkingWidth,
+                walkingColor,
+                null,
+                clickable,
+                JointType.DEFAULT,
+                startCap,
+                endCap
+        );
+    }
+
+    public static ArrayList<PolylineOptions> createTransitPolyline(
+            @NonNull Context context,
+            @Nullable List<Step> stepList,
+            @Dimension(unit = DP) int transitWidth,
+            @ColorInt int transitColor,
+            @Nullable List<PatternItem> transitPatternItemList,
+            @Dimension(unit = DP) int walkingWidth,
+            @ColorInt int walkingColor,
+            @Nullable List<PatternItem> walkingPatternItemList,
+            boolean clickable,
+            @IntRange(from = JointType.DEFAULT, to = JointType.ROUND) int jointType,
+            @Nullable Cap startCap,
+            @Nullable Cap endCap
+    ) {
         ArrayList<PolylineOptions> polylineOptionsList = new ArrayList<>();
         if (stepList != null && stepList.size() > 0) {
             for (Step step : stepList) {
                 ArrayList<LatLng> directionPointList = new ArrayList<>();
                 convertStepToPosition(step, directionPointList);
                 if (step.isContainStepList()) {
-                    polylineOptionsList.add(createPolyline(context, directionPointList, walkingWidth, walkingColor));
+                    polylineOptionsList.add(createPolyline(
+                            context,
+                            directionPointList,
+                            walkingWidth,
+                            walkingColor,
+                            clickable,
+                            jointType,
+                            startCap,
+                            endCap,
+                            walkingPatternItemList
+                    ));
                 } else {
-                    polylineOptionsList.add(createPolyline(context, directionPointList, transitWidth, transitColor));
+                    polylineOptionsList.add(createPolyline(
+                            context,
+                            directionPointList,
+                            transitWidth,
+                            transitColor,
+                            clickable,
+                            jointType,
+                            startCap,
+                            endCap,
+                            transitPatternItemList
+                    ));
                 }
             }
         }
@@ -100,5 +426,205 @@ public class DirectionConverter {
     private static int dpToPx(Context context, int dp) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         return Math.round(dp * (displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public static class PathOption {
+        ArrayList<LatLng> locationList;
+        int width;
+        int color;
+        boolean clickable;
+        int jointType;
+        Cap startCap;
+        Cap endCap;
+        List<PatternItem> patternItemList;
+
+        public ArrayList<LatLng> getLocationList() {
+            return locationList;
+        }
+
+        public PathOption setLocationList(ArrayList<LatLng> locationList) {
+            this.locationList = locationList;
+            return this;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public PathOption setWidth(@Dimension(unit = DP) int width) {
+            this.width = width;
+            return this;
+        }
+
+        public int getColor() {
+            return color;
+        }
+
+        public PathOption setColor(@ColorInt int color) {
+            this.color = color;
+            return this;
+        }
+
+        public boolean isClickable() {
+            return clickable;
+        }
+
+        public PathOption setClickable(boolean clickable) {
+            this.clickable = clickable;
+            return this;
+        }
+
+        public int getJointType() {
+            return jointType;
+        }
+
+        public PathOption setJointType(@IntRange(from = JointType.DEFAULT, to = JointType.ROUND) int jointType) {
+            this.jointType = jointType;
+            return this;
+        }
+
+        public Cap getStartCap() {
+            return startCap;
+        }
+
+        public PathOption setStartCap(Cap startCap) {
+            this.startCap = startCap;
+            return this;
+        }
+
+        public Cap getEndCap() {
+            return endCap;
+        }
+
+        public PathOption setEndCap(Cap endCap) {
+            this.endCap = endCap;
+            return this;
+        }
+
+        public List<PatternItem> getPatternItemList() {
+            return patternItemList;
+        }
+
+        public PathOption setPatternItemList(List<PatternItem> patternItemList) {
+            this.patternItemList = patternItemList;
+            return this;
+        }
+    }
+
+    public static class TransitPathOption {
+        List<Step> stepList = null;
+        int transitWidth;
+        int transitColor;
+        List<PatternItem> transitPatternItemList;
+        int walkingWidth;
+        int walkingColor;
+        List<PatternItem> walkingPatternItemList;
+        boolean clickable;
+        int jointType;
+        Cap startCap;
+        Cap endCap;
+
+        public List<Step> getStepList() {
+            return stepList;
+        }
+
+        public TransitPathOption setStepList(List<Step> stepList) {
+            this.stepList = stepList;
+            return this;
+        }
+
+        public int getTransitWidth() {
+            return transitWidth;
+        }
+
+        public TransitPathOption setTransitWidth(@Dimension(unit = DP) int transitWidth) {
+            this.transitWidth = transitWidth;
+            return this;
+        }
+
+        public int getTransitColor() {
+            return transitColor;
+        }
+
+        public TransitPathOption setTransitColor(@ColorInt int transitColor) {
+            this.transitColor = transitColor;
+            return this;
+        }
+
+        @Nullable
+        public List<PatternItem> getTransitPatternItemList() {
+            return transitPatternItemList;
+        }
+
+        public TransitPathOption setTransitPatternItemList(@Nullable List<PatternItem> transitPatternItemList) {
+            this.transitPatternItemList = transitPatternItemList;
+            return this;
+        }
+
+        public int getWalkingWidth() {
+            return walkingWidth;
+        }
+
+        public TransitPathOption setWalkingWidth(@Dimension(unit = DP) int walkingWidth) {
+            this.walkingWidth = walkingWidth;
+            return this;
+        }
+
+        public int getWalkingColor() {
+            return walkingColor;
+        }
+
+        public TransitPathOption setWalkingColor(@ColorInt int walkingColor) {
+            this.walkingColor = walkingColor;
+            return this;
+        }
+
+        @Nullable
+        public List<PatternItem> getWalkingPatternItemList() {
+            return walkingPatternItemList;
+        }
+
+        public TransitPathOption setWalkingPatternItemList(@Nullable List<PatternItem> walkingPatternItemList) {
+            this.walkingPatternItemList = walkingPatternItemList;
+            return this;
+        }
+
+        public boolean isClickable() {
+            return clickable;
+        }
+
+        public TransitPathOption setClickable(boolean clickable) {
+            this.clickable = clickable;
+            return this;
+        }
+
+        public int getJointType() {
+            return jointType;
+        }
+
+        public TransitPathOption setJointType(@IntRange(from = JointType.DEFAULT, to = JointType.ROUND) int jointType) {
+            this.jointType = jointType;
+            return this;
+        }
+
+        @Nullable
+        public Cap getStartCap() {
+            return startCap;
+        }
+
+        public TransitPathOption setStartCap(@Nullable Cap startCap) {
+            this.startCap = startCap;
+            return this;
+        }
+
+        @Nullable
+        public Cap getEndCap() {
+            return endCap;
+        }
+
+        public TransitPathOption setEndCap(@Nullable Cap endCap) {
+            this.endCap = endCap;
+            return this;
+        }
     }
 }
