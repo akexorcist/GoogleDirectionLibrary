5c517a9fec94e4b1794fe8d461675bfd10a998f1
Akexorcist <akexorcist@gmail.com>
2019-10-13 18:33:45 +0700

    Migrate to AndroidX

--- app/src/main/java/com/akexorcist/googledirection/sample/MainActivity.java
+++ app/src/main/java/com/akexorcist/googledirection/sample/MainActivity.java
@@ -1,9 +1,9 @@
 package com.akexorcist.googledirection.sample;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
