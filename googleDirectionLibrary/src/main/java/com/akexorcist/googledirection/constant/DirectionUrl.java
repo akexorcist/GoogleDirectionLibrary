815051423a6ead082697962c179ce50ad63c759c
nurdinm <muhammadnurdin460@gmail.com>
2021-01-18 13:57:49 +0700

    change url

--- googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
+++ googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
@@ -22,7 +22,8 @@ package com.akexorcist.googledirection.constant;
  * Created by Akexorcist on 11/29/15 AD.
  */
 public class DirectionUrl {
-    public static final String MAPS_API_URL = "https://maps.googleapis.com/maps/api/";
+    public static final String MAPS_API_URL = "https://apimart.pempekdehade.com/konsumen/v1/";
     public static final String DIRECTION_API_URL = "directions/json";
+
 
 }
